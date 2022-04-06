04671ccda7e6c0d6cada60180095b72e3bc946f1
Jesterlopez <jesmark1999@gmail.com>
2022-04-06 11:10:57 -0600

    added checker update

--- functions.php
+++ functions.php
@@ -7,3 +7,18 @@ include_once(plugin_dir_path( __FILE__ ) . 'includes/add-menu-page.php');
 include_once(plugin_dir_path( __FILE__ ) . 'includes/generate-shortcode.php');
 
 include_once(plugin_dir_path( __FILE__ ) . 'includes/page-settings.php');
+
+require 'plugin-update-checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://github.com/Jesterlopez/plugin-wp',
+	__FILE__,
+	'plugin-wp'
+);
+
+$myUpdateChecker->getVcsApi()->enableReleaseAssets();
+
+// //Set the branch that contains the stable release.
+// $myUpdateChecker->setBranch('stable-branch-name');
+
+// //Optional: If you're using a private repository, specify the access token like this:
+// $myUpdateChecker->setAuthentication('your-token-here');
